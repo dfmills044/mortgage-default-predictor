bee6441e785c5338cf1e76461291a90b58cdb4f6
Duncan Mills <duncanfmills@gmail.com>
2026-01-20 21:12:21 -0500

    Created component functions for the aggregator pipeline in data_aggregation_performance.py

--- src/etl/data_aggregation_performance_spark.py
+++ src/etl/data_aggregation_performance_spark.py
@@ -2,7 +2,7 @@ from pyspark.sql import DataFrame
 from pyspark.sql import functions as F
 from pyspark.sql.functions import (col, sum as spark_sum, count as spark_count, mode as lit, lag, coalesce, when)
 from pyspark.sql.window import Window
-from pyspark.sql.types import ByteType
+from pyspark.sql.types import ByteType, IntegerType
 
 window_spec = Window.partitionBy('LOAN_SEQUENCE_NUMBER').orderBy('MONTHLY_REPORTING_PERIOD')
 
@@ -90,3 +90,68 @@ def create_default_col(df_spark: DataFrame) -> DataFrame:
         ).otherwise(lit(0)).cast(ByteType())
     )
     return df_spark
+
+# Function for mapping exit categories
+def get_exit_category_logic(col_name):
+    return when(col(col_name).isin(['THIRD_PARTY_SALE', 'SHORT_SALE_OR_CHARGE_OFF', 'REO_DISPOSITION', 'WHOLE_LOAN_SALE']), 'Default') \
+        .when(col(col_name) == 'PREPAID_OR_MATURED', 'Prepaid') \
+            .when(col(col_name).isin(['REPERFORMING_LOAN_SECURITIZATION', 'DEFECT_PRIOR_TO_OTHER_EVENT']), 'Repurchased') \
+                .otherwise('Active')
+
+# Function for creating column tracking loan age (independent of loan modifications)
+def create_chrono_age_col(df_spark: DataFrame) -> DataFrame:
+    # Identify observation start date for each loan
+    start_dates = df_spark.groupBy('LOAN_SEQUENCE_NUMBER').agg(F.min('MONTHLY_REPORTING_PERIOD').alias('START_DATE'))
+
+    # Join back and calculate a reset-proof age
+    return df_spark.join(start_dates, on='LOAN_SEQUENCE_NUMBER') \
+        .withColumn('CHRONO_AGE', F.round(F.months_between(col('MONTHLY_REPORTING_PERIOD'), col('START_DATE'))).cast(IntegerType()))
+
+# Function for creating default labels (necessary as these columns depend on the entire loan history, not just pre-default)
+def create_default_labels(df_spark: DataFrame) -> DataFrame:
+    # Determine default labels for each loan, along with zero balance aggregations and delinquent interest aggregations
+    # After aggregation, combine this with the aggregated features
+    # df_agg_features.join(labels_df, on="LOAN_SEQUENCE_NUMBER", how="inner")
+    return df_spark.groupBy('LOAN_SEQUENCE_NUMBER').agg(
+        F.max('IS_IN_DEFAULT').alias('LABEL_DEFAULT'),
+        F.sum('IS_IN_DEFAULT').alias('TOTAL_MONTHS_IN_DEFAULT'),
+        F.min(
+            when(col('IS_IN_DEFAULT') == 1, col('CHRONO_AGE'))
+        ).alias('AGE_AT_FIRST_DEFAULT'),
+        F.last('IS_IN_DEFAULT').alias('IS_TERMINAL_DEFAULT'),
+        F.last('ZERO_BALANCE_CODE').alias('ACTUAL_TERMINAL_ZBC'),
+        F.last(get_exit_category_logic('ZERO_BALANCE_CODE')).alias('ACTUAL_EXIT_CATEGORY'),
+        F.max('DELINQUENT_ACCRUED_INTEREST').alias('TOTAL_ACTUAL_LOSS_AMOUNT'),
+        F.max('HAS_DELINQUENT_INTEREST').alias('WAS_CREDIT_LOSS_EVENT'),
+        F.last('DELINQUENCY_DUE_TO_DISASTER').alias('FINAL_EXIT_WAS_DISASTER_RELATED')
+    )
+
+# Function for creating mask of data before default (necessary as the model should be trained only on pre-default data)
+def create_pre_default_mask(df_spark: DataFrame) -> DataFrame:
+    # Find first month each loan defaulted
+    first_default_df = df_spark.filter(col('IS_IN_DEFAULT') == 1) \
+        .groupBy('LOAN_SEQUENCE_NUMBER') \
+            .agg(F.min('MONTHLY_REPORTING_PERIOD').alias('FIRST_DEFAULT'))
+
+    # Join back to main dataframe
+    df_with_cutoff = df_spark.join(first_default_df, on='LOAN_SEQUENCE_NUMBER', how='left')
+
+    # Create mask: Keep rows where reporting period < first default month
+    # If loan never defaulted (FIRST_DEFAULT is null), keep all rows
+    # Apply aggregation to this mask, not the original cleaned df
+    return df_with_cutoff.filter(
+        (col('FIRST_DEFAULT').isNull()) |
+        (col('MONTHLY_REPORTING_PERIOD') < col('FIRST_DEFAULT'))
+    )
+
+# Function for aggregating performance data (given bad rows have been dropped)
+def aggregate_and_join(
+    pre_default_mask: DataFrame,
+    default_labels: DataFrame,
+    agg_functions
+):
+    # Perform heavy lifting (aggregation)
+    df_agg = pre_default_mask.groupBy('LOAN_SEQUENCE_NUMBER').agg(*agg_functions)
+
+    # Grand Join (Not originations yet)
+    return df_agg.join(default_labels, on='LOAN_SEQUENCE_NUMBER', how='inner')
